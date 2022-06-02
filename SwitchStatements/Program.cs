d3f6e225e49a17b5a8986dd9377dc0cfc0326b51
Ernest Acosta <ernestacosta@icloud.com>
2022-06-02 13:01:14 -0700

    final revision

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -15,13 +15,20 @@ namespace SwitchStatements
                     Console.WriteLine("Math is awesome!");
                     break;
                 case "science":
+                case "physics":
+                case "chemistry":
                     Console.WriteLine("Science is tough!");
                     break;
                 case "english":
-                    Console.WriteLine("English is boring!");
+                case "grammar":
+                    Console.WriteLine("I have English!");
                     break;
                 case "pe":
+                case "physical education":
                     Console.WriteLine("PE is fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is boring!");
                     break;
                 default:
                     Console.WriteLine($"Ah I haven't taken that subject before. {subject} sounds exciting!");
